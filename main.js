4ea7e04ac306f6562037d5ee1c7b8be9d29c9f2b
younusmahmood <younus23@gmail.com>
2017-12-13 18:31:30 -0600

    Design updates

---
+++ main.js
@@ -0,0 +1,15 @@
+$(".enter a[href^='#']").on("click", function(e) {
+  e.preventDefault();
+
+  var hash = this.hash;
+
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    400,
+    function() {
+      window.location.hash = hash;
+    }
+  );
+});
